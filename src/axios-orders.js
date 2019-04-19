bb692b40b004dc3c7da9e2756a8b056a54566eae
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-19 10:55:43 +0900

    Sending a POST Request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseUrl: 'https://react-my-burger-7b3f9.firebaseio.com/'
+    baseURL: 'https://react-my-burger-7b3f9.firebaseio.com/'
 });
 
 export default instance;
