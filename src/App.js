47a0f4c980391d57c822caf81d534acd806e0801
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-19 18:41:55 +0900

    Setting Up Routing & Routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
-            <Route path="/" component={BurgerBuilder} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
