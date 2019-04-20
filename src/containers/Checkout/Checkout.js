d0fe8c78c9c58e8a487bbe24eea18ee0e3d297e2
TruongNM <manhtruonghedspi@gmail.com>
2019-04-21 08:48:50 +0900

    Navigation to the Checkout Page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,10 +12,22 @@ class Checkout extends Component {
         }
     }
 
+    checkoutCancelled = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelled}
+                    checkoutContinued={this.checkoutContinuedHandler}
+                />
             </div>
         )
     }
