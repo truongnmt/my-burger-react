82eced227addd61911db41e8c15a1366b47bb26d
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-22 12:44:25 +0900

    Outputing the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -14,7 +14,15 @@ const order = (props) => {
     }
 
     const ingredientOutput = ingredients.map(ig => {
-        return <span>{ig.name} ({ig.amount})</span>;
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>;
     });
 
     return (
