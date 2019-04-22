85b8a067cad5a13939f0dd0336a2750382670c74
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-22 18:15:24 +0900

    Fixing a common validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,7 @@ class ContactData extends Component {
     checkValidity(value, rules) {
         let isValid = false
         if (rules.required) {
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid && isValid;
         }
 
         if (rules.minLength) {
