8b5b81d4c91e9f8283fe6c4e096194abba330fa1
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-22 19:06:45 +0900

    Adding Validation Feedback

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -108,16 +108,16 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        let isValid = false
+        let isValid = false;
         if (rules.required) {
-            isValid = value.trim() !== '' && isValid && isValid;
+            isValid = value.trim() !== '';
         }
 
         if (rules.minLength) {
-            isValid = value.length >= rules.minLength
+            isValid = value.length >= rules.minLength;
         }
         if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength
+            isValid = value.length <= rules.maxLength;
         }
 
         return isValid;
@@ -133,6 +133,7 @@ class ContactData extends Component {
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({ orderForm: updatedOrderForm });
     }
 
@@ -152,6 +153,8 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
                 <Button btnType="Success">ORDER</Button>
