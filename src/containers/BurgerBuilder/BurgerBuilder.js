b0b20688084825331c1ecf7dbc9694c0f4be6ec2
TruongNM <nguyen.manh.truong@mf-financial.jp>
2019-04-23 14:19:05 +0900

    Finishing the Reducer for Ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,13 +31,13 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://react-my-burger-7b3f9.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data });
-            })
-            .catch(error => {
-                this.setState({ error: true })
-            });
+        // axios.get('https://react-my-burger-7b3f9.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ ingredients: response.data });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ error: true })
+        //     });
     }
 
     updatePurchaseState(updatedIngredients) {
